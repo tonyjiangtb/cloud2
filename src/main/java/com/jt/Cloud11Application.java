4f0110aa719ceb2e28347ce1a818c599a463e04a
tonyjiangtb <tonyjiangtb@gmail.com>
2018-03-14 23:02:05 +0800

    edit

--- src/main/java/com/jt/Cloud11Application.java
+++ src/main/java/com/jt/Cloud11Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Cloud11Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//
 		SpringApplication.run(Cloud11Application.class, args);
 	}
 }
